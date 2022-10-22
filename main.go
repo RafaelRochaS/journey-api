18f139cd9b234d7e237740acfcff6fb7e5b296d0
rafael <rafaelrochasouza@outlook.com.br>
2022-10-22 18:37:54 -0300

    add: middleware

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-package journeyapi
+package main
+
+import (
+	"github.com/RafaelRochaS/journey-api/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.New()
+	server.SetTrustedProxies([]string{"192.168.15.1"})
+
+	setMiddleware(server)
+	routes.InitializeRoutes(server)
+
+	server.Run(":8080")
+}
+
+func setMiddleware(server *gin.Engine) {
+	server.Use(gin.Logger())
+	server.Use(gin.Recovery())
+}
